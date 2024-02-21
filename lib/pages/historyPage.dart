27c8e72377c0c2a1f6075efa3829cc8b50167162
aykhanallahverdizade <20290882@ogrenci.ankara.edu.tr>
2024-02-21 12:20:24 +0300

    calendar for appbar

---
+++ lib/pages/historyPage.dart
@@ -0,0 +1,60 @@
+import 'dart:ui';
+import 'dart:math';
+import 'package:calendar_appbar/calendar_appbar.dart';
+import 'package:flutter/material.dart';
+import '../services/data.dart';
+
+class MainHistory extends StatefulWidget {
+  const MainHistory({super.key});
+
+  @override
+  State<MainHistory> createState() => _MainHistoryState();
+}
+
+class _MainHistoryState extends State<MainHistory> {
+  DateTime? selectedDate;
+  Random random = new Random();
+
+  @override
+  void initState() {
+    setState(() {
+      selectedDate = DateTime.now();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CalendarAppBar(
+        onDateChanged: (date) {
+          setState(() async {
+            print(date);
+            // dateData(this., intensity, type, sideEffects)
+
+            await StorageServices.saveData(
+                date.toString(),
+                dateData(date.toString(), random.nextInt(10), "Migraine",
+                    ["Nausea", "Vomiting"]));
+          });
+        },
+        lastDate: DateTime.now(),
+        events: List.generate(
+            100,
+            (index) => DateTime.now()
+                .subtract(Duration(days: index * random.nextInt(5)))),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text("Selected Date: ${selectedDate.toString()}"),
+          ElevatedButton(
+            onPressed: () async {},
+            child: Text("Add a new event data"),
+          ),
+        ],
+      )),
+    );
+  }
+}
