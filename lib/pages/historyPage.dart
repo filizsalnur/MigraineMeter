ce43824366d0b2535a61eb5bec08387837fa7650
aykhanallahverdizade <20290882@ogrenci.ankara.edu.tr>
2024-02-21 20:48:08 +0300

    fix

--- lib/pages/historyPage.dart
+++ lib/pages/historyPage.dart
@@ -36,7 +36,8 @@ class _MainHistoryState extends State<MainHistory> {
         accent: Colors.purple.shade900,
         backButton: false,
         onDateChanged: (date) {
-          StorageServices.loadData(CalendarPageState().formatDateToString(date)).then((value) {
+          StorageServices.loadData(CalendarPageState().formatDateToString(date))
+              .then((value) {
             print(value);
             setState(() {
               selectedDate = date;
@@ -44,33 +45,106 @@ class _MainHistoryState extends State<MainHistory> {
           });
         },
         lastDate: DateTime.now(),
-        events: List.generate(100, (index) => DateTime.now().subtract(Duration(days: index * random.nextInt(5)))),
+        events: List.generate(
+            100,
+            (index) => DateTime.now()
+                .subtract(Duration(days: index * random.nextInt(5)))),
       ),
       body: FutureBuilder<List<String>>(
-        future: StorageServices.loadData(selectedDate!.toString().substring(0, 10)),
+        future:
+            StorageServices.loadData(selectedDate!.toString().substring(0, 10)),
         builder: (context, AsyncSnapshot<List<String>> snapshot) {
-         
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                 List<String> dataParts = snapshot.data![index].split(',');
-                return Container(
-                  height: 250,
-                  color: Colors.purple.shade900,
-                   child: Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: dataParts.map((part) {
-                        return ListTile(
-                          title: Text(removeBracketsAndBraces(part.trim()), style: TextStyle(fontSize: 25),), 
-                        );
-                      }).toList(),
-                    ),
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              List<String> dataParts = snapshot.data![index].split(',');
+              return Container(
+                height: 250,
+                color: Colors.purple.shade900,
+                child: Card(
+                  color: Colors.deepPurpleAccent,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: dataParts.map((part) {
+                      print("part $part");
+                      if (part.contains('intensity')) {}
+                      if (part.contains('type')) {
+                        if (part.contains('part1')) {
+                          return Image.asset(
+                            'assets/b1.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part2')) {
+                          return Image.asset(
+                            'assets/b2.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part3')) {
+                          return Image.asset(
+                            'assets/b3.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part4')) {
+                          return Image.asset(
+                            'assets/b4.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part5')) {
+                          return Image.asset(
+                            'assets/b5.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part6')) {
+                          return Image.asset(
+                            'assets/b6.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part7')) {
+                          return Image.asset(
+                            'assets/b7.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part8')) {
+                          return Image.asset(
+                            'assets/b8.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part9')) {
+                          return Image.asset(
+                            'assets/b9.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        } else if (part.contains('part10')) {
+                          return Image.asset(
+                            'assets/b10.png',
+                            width: 100,
+                            height: 100,
+                          );
+                        }
+                      }
+
+                      if (part.contains('symptoms')) {}
+                      return ListTile(
+                        title: Text(
+                          removeBracketsAndBraces(part.trim()),
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      );
+                    }).toList(),
                   ),
-                );
-              },
-            );
-          
+                ),
+              );
+            },
+          );
         },
       ),
       bottomNavigationBar: BottomBar(pageName: "history"),
