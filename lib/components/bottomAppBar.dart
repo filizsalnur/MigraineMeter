1d13e5c07f52e08e5d5837623e7d24575c56e4db
Filiz Salnur <filizsalnur@gmail.com>
2024-02-21 12:20:24 +0300

    bottom app bar

---
+++ lib/components/bottomAppBar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  final String pageName;
+  const BottomBar({super.key,
+  required this.pageName,
+  });
+
+  @override
+  State<BottomBar> createState() => BottomBarState();
+}
+
+class BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      height: 70,
+      color: Colors.white,
+      elevation: 100,
+      shape: const CircularNotchedRectangle(),
+
+      child:Padding(padding: EdgeInsets.all(0),
+      child: Column(
+        children: [
+          SizedBox(height: 10,),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+           
+            ElevatedButton(
+                onPressed: () {},
+                child: Icon(Icons.home, size: 40,color: (widget.pageName =="home") ?Colors.white: Colors.black),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                    elevation: MaterialStateProperty.all(0),
+                  backgroundColor: (widget.pageName =="home") ? MaterialStateProperty.all(Colors.purple.shade900):MaterialStateProperty.all(Colors.white), 
+                 
+                ),
+              ),
+            ElevatedButton(
+                onPressed: () {},
+                child: Icon(Icons.history, size: 40,color: (widget.pageName =="history") ?Colors.white: Colors.black),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                    elevation: MaterialStateProperty.all(0),
+                  backgroundColor: (widget.pageName =="history") ? MaterialStateProperty.all(Colors.purple.shade900):MaterialStateProperty.all(Colors.white), 
+                 
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Icon(Icons.settings, size: 40,color: (widget.pageName =="settings") ?Colors.white: Colors.black,),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: (widget.pageName =="settings") ? MaterialStateProperty.all(Colors.purple.shade900):MaterialStateProperty.all(Colors.white),
+                 
+                ),
+              )
+                      
+          ]),
+        ],
+      ),)
+    );
+  }
+}
